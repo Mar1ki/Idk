e698024586a5d6f8651221be6c0d4f22f78fc964
Mariki <68015763+Marikider@users.noreply.github.com>
2024-06-29 15:00:43 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,19 @@
+#include <iostream>
+
+#include <vector>
+using namespace std;
+
 vector<int> Increment(vector<int> orig, int inc){
   vector<int> newv;
   for( auto i: orig){
     newv.push_back(i+inc);
    }
   return newv;
+}
+int main(){
+  vector<int> a=vector<int>{1,2,3,4,5};
+  for (auto i: Increment(a,1)){
+    cout<<i<<" ";
+  }
+  return 0;
 }
